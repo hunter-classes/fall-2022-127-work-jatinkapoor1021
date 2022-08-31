5873b5f1a8d9ccb468a716196527c45d74cbee2e
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-08-31 19:31:16 -0400

    Update and rename sent to sent.py

---
+++ chapter-2/sent.py
@@ -0,0 +1,33 @@
+
+5. 
+word1 = "All"
+word2 = "work"
+word3 = "and"
+word4 = "no"
+word5 = "play"
+word6 = "makes"
+word7 = "Jack"
+word8 = "a"
+word9 = "dull"
+word10 = "boy."
+
+print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
+
+6. 6*1-2 becomes 6*(1-2)=-6
+
+
+7. 
+P = 10000
+n = 12
+r = 0.08
+
+t = int(input("Compound for how many years? "))
+
+final = P * ( ((1 + (r/n)) ** (n * t)) )
+
+print ("The final amount after", t, "years is", final)
+
+8. 
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
