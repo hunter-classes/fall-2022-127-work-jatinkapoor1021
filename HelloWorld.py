b1f9b243272c9bfb0650cc6416f4245c05272b5a
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-09-12 09:34:14 -0400

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+s1="Hello World"
+s2= 'another\nstring'
+
+s3 = """
+This is a multiline string we use the triple quotes for those
+"""
+
+s4=s1+s1
+
+print(s4)
+print(s1*3)
+print(3*s1)
+
+print(len(s1))
+print(len("abcde"))
