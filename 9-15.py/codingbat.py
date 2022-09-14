6d893edd87af1d00925023592387a956d5d424eb
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-09-13 20:35:08 -0400

    Update codingbat.py

--- 9-15.py/codingbat.py
+++ 9-15.py/codingbat.py
@@ -1,1 +1,26 @@
+hello_Name: 
+public String helloName(String name) {
 
+  return "Hello " + name + "!";
+
+}
+
+make_out_word: 
+  public String makeOutWord(String out, String word) {
+    return out.substring(0, 2) + word + out.substring(2);
+}
+
+first_two:
+  public String firstTwo(String str) {
+
+  int len = str.length();
+
+  if (len < 2)
+
+    return str;
+
+  else
+
+    return str.substring(0,2);
+
+}
