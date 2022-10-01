8436652fda95fa32755bbc539f691ec099bb6101
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-10-01 18:48:14 -0400

    Update list1.py

--- chapter 10/list1.py
+++ chapter 10/list1.py
@@ -1,1 +1,15 @@
+Exercise 3:
+  
+myList = [76, 92.3, 'hello', True, 4, 76]
 
+myList.append("apple")         
+myList.append(76)              
+myList.insert(3, "cat")        
+myList.insert(0, 99)          
+
+print(myList.index("hello"))   
+print(myList.count(76))        
+myList.remove(76)              
+myList.pop(myList.index(True)) 
+
+print (myList)
