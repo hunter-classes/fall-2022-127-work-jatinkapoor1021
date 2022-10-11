3549c0ec2025850507ec27cb8830d0edde9f3ce6
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-10-10 22:55:22 -0400

    Create added.py

---
+++ listlab.py/added.py
@@ -0,0 +1,12 @@
+def addlist(list1, list2):
+    newlist = []
+    for i in range(0, len(list1)):
+        newlist.append(list1[i] + list2[i])
+        
+    return newlist
+
+# calling fnc addlist
+list1 = [1,2,3]
+list2 = [10,20,30]
+addlist = addlist(list1, list2)
+print(addlist) 
