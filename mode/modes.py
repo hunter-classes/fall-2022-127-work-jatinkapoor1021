cb57a0b1d556870f1650fd7bddbcab5df7ec72dc
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2022-10-23 19:40:56 -0400

    Update modes.py

--- mode/modes.py
+++ mode/modes.py
@@ -1,1 +1,24 @@
 
+First function:
+  
+  
+
+list1 = [20, 5, 8, 63, 44, 85]
+ 
+list1.sort()
+print("Smallest element is:", list1[0])
+
+
+
+Second function:
+  
+def frequency(l, v):
+    count = 0
+     
+    for i in l:
+        if i == v: count += 1
+    return count
+ 
+l = [1, 2, 2, 3, 6, 2, 4]
+v = 2
+print(frequency(l, v))
